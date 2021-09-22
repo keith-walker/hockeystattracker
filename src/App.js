e90036938194593da7bed359d9a1ed3a2c0d36ed
Keith <keithwalker@callutheran.edu>
2021-09-21 22:03:28 -0700

    Added in hooks, printing out data to web page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
@@ -7,7 +7,8 @@ const api=axios.create({
   baseURL: 'https://statsapi.web.nhl.com/api/v1/people/8474141/stats?stats=statsSingleSeason&season=20202021'
 })
 
-let stats;
+let stats1;
+let test;
 
 class App extends Component {
     state = {
@@ -16,20 +17,24 @@ class App extends Component {
     constructor(){
       super();
       api.get('').then(res=>{
-        console.log(res.data)
-        stats=res.data.stats[0].splits[0];
+        //console.log(res.data)
+        test=res.data.stats[0].splits[0].season;
+        console.log(test);
+        let stats=JSON.stringify(res.data.stats[0].splits[0], null, 2);
         console.log(stats);
-        this.state=({stats: res.data.stats[0].splits[0]});
-        console.log(this.state);
+        this.state=({stats1: res.data.stats[0].splits[0]});
+        //console.log(this.state);
       })
     }
 
   render() {
     return (
       <div className="App">
-        {stats}
+        <pre> {JSON.stringify(stats1, null, 2)} </pre>
+        <b> {test} </b>
       </div>
     );
   }
+
 }
 export default App;
