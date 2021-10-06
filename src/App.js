5485d2b87a35643aceca3f0ca3730222e99bc60b
Keith <keithwalker@callutheran.edu>
2021-10-06 10:24:03 -0700

    Added initializing hashmap of player name and player keys

--- src/App.js
+++ src/App.js
@@ -2,17 +2,44 @@ import "./App.css";
 import axios from "axios";
 import { useState } from "react";
 import { BasicTable } from "./table";
+import HashMap from "hashmap";
 
-const API_URL =
+const getPlayerStats =
   "https://statsapi.web.nhl.com/api/v1/people/8474141/stats?stats=statsSingleSeason&season=20202021";
+
+const getTeamsAndRosters =
+  "https://statsapi.web.nhl.com/api/v1/teams?expand=team.roster";
 
 function App() {
   const [data, setData] = useState([]);
+  const [cachedData, cacheData] = useState([]);
 
   let apiCall = async () => {
     try {
-      let data = await axios.get(API_URL);
+      let data = await axios.get(getPlayerStats);
       setData(data.data.stats[0].splits[0]);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  let init = async () => {
+    try {
+      let cachedData = await axios.get(getTeamsAndRosters);
+      let teams = cachedData.data.teams;
+      let initMap = new HashMap();
+      //teams is an array, loop through this and to teams.roster.roster
+      //roster is also an array, loop through this array and access the .person element in each element and see if the .fullName matches the one we are looking for
+      //cache the roster[i].person.fullName as the key, and the roster[i].person as the value
+      for (let i = 0; i < teams.length; i++) {
+        let roster = teams[i].roster.roster;
+        for (let j = 0; j < roster.length; j++) {
+          initMap.set(roster[j].person.fullName, roster[j].person);
+        }
+      }
+
+      console.log(initMap);
+      console.log(initMap.get("Aaron Dell"));
     } catch (err) {
       console.log(err);
     }
@@ -20,8 +47,9 @@ function App() {
 
   return (
     <div className="App">
-      <BasicTable />
-      <button onClick={apiCall}>Click me</button>
+      {/* <BasicTable /> */}
+      <button onClick={init}>Initialize</button>
+      <button onClick={apiCall}>Get Player</button>
       <pre> {JSON.stringify(data, null, 2)} </pre>
     </div>
   );
