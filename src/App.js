eb957ecc721815de40f688e564275c3a3ca64b13
Keith <keithwalker@callutheran.edu>
2021-10-27 18:33:15 -0700

    Added in method that makes the initial extra API call

--- src/App.js
+++ src/App.js
@@ -17,11 +17,41 @@ function App() {
 
   let apiCall = async () => {
     try {
+      var start = Date.now();
       let queryParams = "/stats?stats=statsSingleSeason&season=";
       let player = initMap.get(playerName);
       let getPlayerStats = API_URL + player.link + queryParams + season;
       let data = await axios.get(getPlayerStats);
       setData(data.data.stats[0].splits[0]);
+      console.log(Date.now() - start);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  let extraApiCall = async () => {
+    try {
+      var start = Date.now();
+      //make initial call to get team roster data
+      let teamRosterData = await axios.get(getTeamsAndRosters);
+      let teams = teamRosterData.data.teams;
+      //teams is an array, loop through this and to teams[i].roster.roster
+      //roster is also an array, loop through this array and access the .person element in each element
+      //if roster[i].person.fullName is the name we want, break out and use the roster[i].person.
+      let playerLink = "";
+      for (let i = 0; i < teams.length; i++) {
+        let roster = teams[i].roster.roster;
+        for (let j = 0; j < roster.length; j++) {
+          if (roster[j].person.fullName === playerName) {
+            playerLink = roster[j].person.link;
+          }
+        }
+      }
+      let queryParams = "/stats?stats=statsSingleSeason&season=";
+      let getPlayerStats = API_URL + playerLink + queryParams + season;
+      let data = await axios.get(getPlayerStats);
+      setData(data.data.stats[0].splits[0]);
+      console.log(Date.now() - start);
     } catch (err) {
       console.log(err);
     }
@@ -61,7 +91,8 @@ function App() {
         </label>
       </form>
 
-      <button onClick={apiCall}>Get Player</button>
+      <button onClick={apiCall}>1 API Call</button>
+      <button onClick={extraApiCall}>2 API Calls</button>
       <pre> {JSON.stringify(data, null, 2)} </pre>
     </div>
   );
