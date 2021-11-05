6dcf232063959ed3604229cb474c08e199140dab
Keith <keithwalker@callutheran.edu>
2021-11-05 10:15:27 -0700

    Added some style to the tool/search bar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,19 @@ import "./App.css";
 import axios from "axios";
 import { useState, useEffect } from "react";
 import HashMap from "hashmap";
+import {
+  Button,
+  Toolbar,
+  AppBar,
+  Typography,
+  TextField,
+  Box,
+} from "@material-ui/core";
+import MaUTable from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
 
 const API_URL = "https://statsapi.web.nhl.com";
 
@@ -9,7 +22,7 @@ const getTeamsAndRosters = API_URL + "/api/v1/teams?expand=team.roster";
 
 let initMap = new HashMap();
 
-let season = "20202021";
+let season = "20212022";
 
 function App() {
   const [data, setData] = useState([]);
@@ -23,7 +36,7 @@ function App() {
       let getPlayerStats = API_URL + player.link + queryParams + season;
       let data = await axios.get(getPlayerStats);
       setData(data.data.stats[0].splits[0]);
-      console.log(Date.now() - start);
+      console.log("1 call = " + (Date.now() - start) + "ms");
     } catch (err) {
       console.log(err);
     }
@@ -38,20 +51,31 @@ function App() {
       //teams is an array, loop through this and to teams[i].roster.roster
       //roster is also an array, loop through this array and access the .person element in each element
       //if roster[i].person.fullName is the name we want, break out and use the roster[i].person.
+      let innerloop = 0;
+      let outerloop = 0;
       let playerLink = "";
       for (let i = 0; i < teams.length; i++) {
+        outerloop++;
         let roster = teams[i].roster.roster;
         for (let j = 0; j < roster.length; j++) {
+          innerloop++;
           if (roster[j].person.fullName === playerName) {
             playerLink = roster[j].person.link;
+            break;
           }
         }
+        if (playerLink !== "") {
+          break;
+        }
       }
+      console.log("innerloop= " + innerloop);
+      console.log("outerloop= " + outerloop);
       let queryParams = "/stats?stats=statsSingleSeason&season=";
       let getPlayerStats = API_URL + playerLink + queryParams + season;
       let data = await axios.get(getPlayerStats);
       setData(data.data.stats[0].splits[0]);
-      console.log(Date.now() - start);
+      console.log(data.data.stats[0].splits[0]);
+      console.log("2 calls = " + (Date.now() - start) + "ms");
     } catch (err) {
       console.log(err);
     }
@@ -66,12 +90,18 @@ function App() {
         //teams is an array, loop through this and to teams[i].roster.roster
         //roster is also an array, loop through this array and access the .person element in each element
         //cache the roster[i].person.fullName as the key, and the roster[i].person as the value
+        let outerloop = 0;
+        let innerloop = 0;
         for (let i = 0; i < teams.length; i++) {
+          outerloop++;
           let roster = teams[i].roster.roster;
           for (let j = 0; j < roster.length; j++) {
+            innerloop++;
             initMap.set(roster[j].person.fullName, roster[j].person);
           }
         }
+        console.log("innerloop= " + innerloop);
+        console.log("outerloop= " + outerloop);
       } catch (err) {
         console.log(err);
       }
@@ -80,19 +110,31 @@ function App() {
 
   return (
     <div className="App">
-      <form>
-        <label>
-          Player Name
-          <input
-            type="text"
-            value={playerName}
-            onChange={(e) => setPlayerName(e.target.value)}
-          ></input>
-        </label>
-      </form>
+      <AppBar position="static" color="default">
+        <Toolbar style={{ justifyContent: "center" }}>
+          <Typography component={"span"} variant={"body2"}>
+            <Box>
+              <TextField
+                label="Player Name"
+                type="text"
+                value={playerName}
+                onChange={(e) => setPlayerName(e.target.value)}
+              ></TextField>
+              <Button color="primary" variant="contained" onClick={apiCall}>
+                1 API Call
+              </Button>
+              <Button
+                color="primary"
+                variant="contained"
+                onClick={extraApiCall}
+              >
+                2 API Calls
+              </Button>
+            </Box>
+          </Typography>
+        </Toolbar>
+      </AppBar>
 
-      <button onClick={apiCall}>1 API Call</button>
-      <button onClick={extraApiCall}>2 API Calls</button>
       <pre> {JSON.stringify(data, null, 2)} </pre>
     </div>
   );
